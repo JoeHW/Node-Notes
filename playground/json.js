2b1f3b6650365ca718c5b5f9e029b1302ea96ed9
Joe Williamson <jhwilliamson@outlook.com>
2017-08-30 19:40:41 -0400

    JSON files

---
+++ playground/json.js
@@ -0,0 +1,31 @@
+// let obj = {
+// name: "Joseph"
+// };
+// let stringObj = JSON.stringify(obj);
+//
+// console.log(typeof stringObj);
+// console.log(stringObj);
+
+// let personString = '{"name": "Andrew","age": 25}';
+// let person = JSON.parse(personString);
+// console.log(typeof person);
+// console.log(person);
+
+const fs = require('fs');
+
+let originalNote = {
+
+  title: 'Some title',
+  body: 'This is the body'
+
+}
+
+let originalNoteString = JSON.stringify(originalNote);
+
+fs.writeFileSync('notes.json', originalNoteString);
+
+let noteString = fs.readFileSync('notes.json');
+let note = JSON.parse(noteString);
+
+console.log(typeof note);
+console.log(note.title);

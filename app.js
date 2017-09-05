5a0b35ac864410f16778365220af4056e225604b
Joe Williamson <jhwilliamson@outlook.com>
2017-09-05 11:21:42 -0400

    finished commands and help

--- app.js
+++ app.js
@@ -4,21 +4,31 @@ const yargs = require('yargs');
 
 const notes = require('./notes.js');
 
+let titleOptions = {
+  describe: 'Title of note',
+  demand: true,
+  alias: 't'
+}
+
 const argv = yargs
 .command('add', 'Add a new note', {
-  title: {
-    describe: 'Title of note',
-    demand: true,
-    alias: 't'
-  },
+  title: titleOptions,
   body: {
     describe: 'Content of note',
     demand: true,
     alias: 'b'
   }
 })
+.command('list', 'Display all notes')
+.command('read', 'Read a note', {
+  title: titleOptions,
+})
+.command('remove', 'Remove a note', {
+  title: titleOptions,
+})
 .help()
 .argv;
+
 let command = argv._[0];
 
 if(command === 'add'){
